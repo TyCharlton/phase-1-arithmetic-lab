3de9ccb98fe2ddaa476242f82a3c458119c8a7d5
TylerCharlton <TyCharlton1993@gmail.com>
2023-12-16 20:13:30 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
+// 1
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+console.log("multiply:", multiply); 
+
+// 2
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+console.log("random:", random);
+
+// 3
+const num3 = 14;
+const num4 = 5;
+const mod = num3 % num4;
+
+console.log("mod:", mod); 
+
+// 4
+const max = Math.max(10, 20, 15, 5);
+
+console.log("max:", max); 
+
+
+ 
+
+
+
+
+
+
